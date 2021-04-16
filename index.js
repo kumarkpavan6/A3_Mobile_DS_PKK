60b694612b0a4ee8e81452fda237cfd44919494f
Pavan <kumarkpavan6@gmail.com>
2021-04-16 07:42:44 -0400

    added get all url + insert url

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const ItemSchema = new Schema({
    description:String,
    goldPerTurn:Number
 })
-const Student = mongoose.model("items_table", ItemSchema)
+const Item = mongoose.model("items_tables", ItemSchema)
 
 // ----------------------------------
 // express setup
@@ -27,6 +27,98 @@ const app = express();
 app.use(express.json())
 const HTTP_PORT = process.env.PORT || 8080;
 
+// ----------------------------------
+// Url endpoints
+// ----------------------------------
+// GET ALL
+app.get("/api/items", (req, res) => {
+    // 1. search the database for items and return them
+    Item.find().exec().then(
+        (results) => {
+            console.log(results)
+            res.status(200).send(results)
+        }
+    ).catch(
+        (err) => {
+            console.log(error)
+            res.status(500).send("Internal Server Error when getting all Items from database.")
+        }
+    )    
+})
+
+app.get("/api/items/:irarity", (req,res) => {
+    // 1. Determine which items the user wants
+    // - by rarity 
+    
+    console.log(`Searching for: ${req.params.irarity}`)
+
+
+    Item.find({rarity:req.params.irarity}).exec()
+        .then(
+            (results) => {
+                console.log(`Result from database: `)
+                console.log(results)
+                if (results === null) {
+                    console.log("record not found")
+                    const msg = {
+                        statusCode:404,
+                        msg: "Record not found"
+                    }
+                    res.status(404).send(msg)
+                }
+                else {
+                    console.log("Item(s) found")                    
+                    res.status(200).send(results)
+                }
+                
+            }
+        ).catch(
+            (err) => {
+                console.log(`Error`)
+                console.log(err)
+                const msg = {
+                    statusCode:500,
+                    msg: "Error when getting items from database."
+                }
+                res.status(500).send(msg)
+
+            }
+        )
+})
+
+
+
+// INSERT 
+app.post("/api/items", (req, res) => {
+
+    // 1. what did the client send us
+    // - what data does the client want us insert into the database
+    console.log("I received this from the client:")
+    console.log(req.body)
+    
+    // 2. Take that information and CREATE someone in your database!
+    // - mongoose
+
+    Item.create(req.body).then(
+        (result) => {
+            //javascript
+            console.log("Create success!")
+            console.log(result)
+            // express
+            res.status(201).send("Insert success!")
+        }
+    ).catch(
+        (err) => {
+            console.log(`Error`)
+            console.log(err)
+            const msg = {
+                statusCode:500,
+                msg: "Error when getting items from database."
+            }
+            res.status(500).send(msg)
+        }
+    )    
+})
 
 
 
